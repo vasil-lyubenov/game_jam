38fcf9338260bc1ab7b7b819a35e331a5d350836
Vasil Lyubenov <vasil.lyubenov@imperiaonline.org>
2026-03-06 10:13:13 +0200

    Added initial state of the employees

---
+++ Assets/Scripts/Data/Structs.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+namespace CEOGame.Data
+{
+    [Serializable]
+    public struct Relationship
+    {
+        public EmployeeData colleague;
+        public RelationshipType type;
+    }
+
+    [Serializable]
+    public struct DecisionOutcome
+    {
+        [Header("Immediate Effects")]
+        public int budgetChange;
+        public int moraleChange;
+        public int peopleChange;
+        public int employeeHappinessChange;
+        public bool employeeLeaves;
+
+        [Header("Position Change")]
+        public bool changesPosition;
+        public Position newPosition;
+
+        [Header("Delayed Effects")]
+        public bool hasDelayedEffect;
+        public int delayedTurns;
+        public int delayedBudgetChange;
+        public int delayedMoraleChange;
+
+        [Header("Feedback")]
+        [TextArea(2, 4)]
+        public string outcomeText;
+    }
+}
